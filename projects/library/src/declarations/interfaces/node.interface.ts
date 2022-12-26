7b6c6c3e4c8fb082089e76d4a6d71e6d6890de19
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-27 00:59:44 +0400

    chore: add JSDoc to Node interface

--- projects/library/src/declarations/interfaces/node.interface.ts
+++ projects/library/src/declarations/interfaces/node.interface.ts
@@ -4,13 +4,95 @@ import type { WithInnerTextTrait } from '../traits/with-inner-text.trait';
 import type { WithTagNameTrait } from '../traits/with-tag-name.trait';
 
 export namespace Node {
+  /**
+   * @example
+   * ### Usage:
+   * ```javascript
+   * {
+   *   tagName: 'div',
+   *   attributes: {
+   *     class: 'container'
+   *   }
+   * }
+   * ```
+   *
+   * ### Result HTML:
+   * ```html
+   * <div class="container"></div>
+   * ```
+   */
   export interface WithTag extends WithTagNameTrait, Partial<WithAttributesTrait> {}
 
+  /**
+   * @example
+   * ### Usage:
+   * ```javascript
+   * {
+   *   tagName: 'section',
+   *   children: [
+   *     { tagName: 'div' },
+   *     { tagName: 'div' },
+   *     { tagName: 'div' }
+   *   ]
+   * }
+   * ```
+   *
+   * ### Result HTML:
+   * ```html
+   * <section>
+   *   <div></div>
+   *   <div></div>
+   *   <div></div>
+   * </section>
+   * ```
+   */
   export interface WithChildren extends WithTag, WithChildrenTrait {}
 
+  /**
+   * @example
+   * ### Usage:
+   * ```javascript
+   * {
+   *   innerText: 'Who is John Galt?'
+   * }
+   * ```
+   *
+   * ### Result HTML:
+   * ```html
+   * Who is John Galt?
+   * ```
+   */
   export type Text = WithInnerTextTrait;
 
+  /**
+   * @example
+   * ### Usage:
+   * ```javascript
+   * {
+   *   tagName: 'a',
+   *   attributes: {
+   *     href: 'https://en.wikipedia.org/wiki/John_Galt'
+   *   },
+   *   innerText: 'Who is John Galt?'
+   * }
+   * ```
+   *
+   * ### Result HTML:
+   * ```html
+   * <a href="https://en.wikipedia.org/wiki/John_Galt">Who is John Galt?</a>
+   * ```
+   */
   export interface TextWithTag extends WithTag, Text {}
 
+  /**
+   * @description
+   * Describes Node with unknown internal structure.
+   *
+   * **Better use:**
+   * - Node.WithTag
+   * - Node.WithChildren
+   * - Node.Text
+   * - Node.TextWithTag
+   */
   export type Any = WithTag | WithChildren | Text | TextWithTag;
 }
