dbaa961544529677f991ed6ef399c4fea5856b98
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-27 02:00:12 +0400

    feature: link JSON editor with HTML

--- projects/demo/src/index.ts
+++ projects/demo/src/index.ts
@@ -1,13 +1,14 @@
 import { editor } from 'monaco-editor';
+import { getElement } from 'library';
 
 const jsonEditorContainer: HTMLElement | null = document.querySelector('section.editor-json');
 if (jsonEditorContainer === null) {
   throw new Error('JSON editor container is missing');
 }
 
-editor.create(jsonEditorContainer, {
-  value: "function hello() {\n\talert('Hello world 1!');\n}",
-  language: 'javascript',
+const jsonEditor: editor.IStandaloneCodeEditor = editor.create(jsonEditorContainer, {
+  value: '',
+  language: 'json',
 });
 
 const htmlEditorContainer: HTMLElement | null = document.querySelector('section.editor-html');
@@ -15,7 +16,20 @@ if (htmlEditorContainer === null) {
   throw new Error('HTML editor container is missing');
 }
 
-editor.create(jsonEditorContainer, {
-  value: "function hello() {\n\talert('Hello world 2!');\n}",
-  language: 'javascript',
+const htmlEditor: editor.IStandaloneCodeEditor = editor.create(htmlEditorContainer, {
+  value: '',
+  language: 'html',
+  readOnly: true,
+});
+
+jsonEditor.onDidChangeModelContent(() => {
+  const stringifiedValue: string = jsonEditor.getValue();
+  const resultElement: HTMLElement | Text = getElement(JSON.parse(stringifiedValue));
+
+  if (resultElement instanceof Text) {
+    htmlEditor.setValue(resultElement.nodeValue ?? '');
+    return;
+  }
+
+  htmlEditor.setValue(resultElement.outerHTML);
 });
